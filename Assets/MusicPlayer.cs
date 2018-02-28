1c7c9c53d248b977d38734e0d5093e4551e1cbd4
Joe Hunt <josephb.hunt@gmail.com>
2018-02-27 21:49:26 -0500

    Initial commit

---
+++ Assets/MusicPlayer.cs
@@ -0,0 +1,25 @@
+﻿using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class MusicPlayer : MonoBehaviour {
+
+    private void Awake()
+    {
+        DontDestroyOnLoad(this);
+    }
+
+    // Use this for initialization
+    void Start () {
+        Invoke("LoadNextLevel", 3f);
+    }
+	
+	// Update is called once per frame
+	void Update () {
+		
+	}
+
+    private void LoadNextLevel()
+    {
+        SceneManager.LoadScene(1);
+    }
+}
